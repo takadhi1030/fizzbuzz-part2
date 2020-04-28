4331ffe946153a7b458e0022672efaf70cd053ad
takashi <tk19490612@gmail.com>
2020-04-28 21:37:36 +0900

    ガード節を採用してみた

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,15 @@
 def fizzbuzz_convert(number):
     if number % 15 == 0:
         return "Fizz Buzz"
-    elif number % 5 == 0:
+
+    if number % 5 == 0:
         return "Buzz"
-    elif number % 3 == 0:
+
+    if number % 3 == 0:
         return "Fizz"
-    else:
+
         return str(number)
+
 
 result = fizzbuzz_convert(1)
 print(result)
